1d4379f14eb447446e97f7db33061585fdc5545b
Shilpi Sharma <sap.developer.shilpi@gmail.com>
2017-12-18 13:42:31 +0100

    Add GET /todo/:ids

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -29,6 +30,23 @@ app.get('/todos', (req,res) => {
     res.status(400).send(err);
   });
 });
+
+// GET todos with id
+
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.status(200).send({todo});
+  });
+  },(err) => {
+    res.status(400).send(err);
+  });
 
 app.listen(3000, () => {
   console.log("started on port 3000");
