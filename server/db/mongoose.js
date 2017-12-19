ceaa4c6d8e6144b4bcc73996a4f8b666abb24f6f
Shilpi Sharma <sap.developer.shilpi@gmail.com>
2017-12-19 13:42:47 +0100

    adding env variable for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;  // used to make it mongoose use Promise
-mongoose.connect('mongodb://shilpi:shilpi@ds161136.mlab.com:61136/todo-app-api');
-//mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.PROD_MONGODB ||'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
 // the above export is same as  { mongoose: mongoose};
