9f8438da4166e20ef88fbd48b138725ae0b0d04f
Shilpi Sharma <sap.developer.shilpi@gmail.com>
2017-12-19 14:20:57 +0100

    setting up diff env for development and test

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;  // used to make it mongoose use Promise
-mongoose.connect(process.env.PROD_MONGODB ||'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.PROD_MONGODB);
 
 module.exports = {mongoose};
 // the above export is same as  { mongoose: mongoose};
